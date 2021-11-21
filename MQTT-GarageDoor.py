5c47030098418e87c7198eaaf5791c2977534108
Steve Lake <steve.lake.au@gmail.com>
2021-11-21 18:52:37 +0800

    Initial Prototype

--- MQTT-GarageDoor.py
+++ MQTT-GarageDoor.py
@@ -1,58 +1,100 @@
 #%%
+from email.errors import CloseBoundaryNotFoundDefect
 import paho.mqtt.client as mqtt
 import RPi.GPIO
 import time
-
+from constants import *
 
 RPi.GPIO.setmode(RPi.GPIO.BCM)
-RPi.GPIO.setup(6,RPi.GPIO.OUT)
-RPi.GPIO.output(6,1)
-RPi.GPIO.setup(23, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
-RPi.GPIO.setup(24, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
+RPi.GPIO.setup(GPIO_TRIGGER,RPi.GPIO.OUT)
+RPi.GPIO.output(GPIO_TRIGGER,1)
+RPi.GPIO.setup(GPIO_DOOROPEN, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
+RPi.GPIO.setup(GPIO_DOORCLOSED, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_UP)
 
+doorState = DOORSTATE_UNKNOWN # unknown
+lastCheck = 0
+#%%
+
+
+client = mqtt.Client(client_id="RPIMQTTGARAGEDOOR")
+
+
+#%%
+def check_door_state(routine=False):
+    global doorState
+    global client
+    global lastCheck
+    if (routine):
+        if ((time.time() - lastCheck) <= 5):
+            print("Too soon - could be just starting an operation")
+            return doorState
+    lastCheck = time.time()
+    if ((RPi.GPIO.input(GPIO_DOOROPEN) != GPIO_INPUT_DEFAULT) and (doorState != DOORSTATE_OPEN)):
+        doorState = DOORSTATE_OPEN
+    elif ((RPi.GPIO.input(GPIO_DOORCLOSED) != GPIO_INPUT_DEFAULT) and (doorState != DOORSTATE_CLOSED)):
+        doorState = DOORSTATE_CLOSED
+    elif ((doorState == DOORSTATE_OPEN) and (RPi.GPIO.input(GPIO_DOOROPEN) == GPIO_INPUT_DEFAULT)):
+        doorState = DOORSTATE_UNKNOWN
+    elif ((doorState == DOORSTATE_CLOSED) and (RPi.GPIO.input(GPIO_DOORCLOSED) == GPIO_INPUT_DEFAULT)):
+        doorState = DOORSTATE_UNKNOWN
+    else:
+        return doorState # Don't publish updates if we don't need to.
+    print("Updating")
+    client.publish(MQTT_CMD_STATUS, MQTT_STATUS[doorState])
+    return doorState
 #%%
 
 def on_connect(client, userdata, flags, rc):
+    global doorState
     print("Connected with result code "+str(rc))
-    client.subscribe("homeassistant/cover/garagedoor/set")
-    client.publish("homeassistant/cover/garagedoor/available","online")
-    client.publish("homeassistant/cover/garagedoor",check_state())
+    client.subscribe(MQTT_CMD_TRIGGER)
+    client.publish(MQTT_CMD_AVAILABILITY, MQQT_PAYLOAD_AVAILABLE)
+    client.publish(MQTT_CMD_STATUS, MQTT_STATUS[check_door_state()])
 
+#%%
 def on_message(client, userdata, msg):
+    global doorState
     print("Received {}, {}, {}, {}".format(msg.topic, msg.payload, msg.qos, msg.retain))
-    if (msg.payload == b'TRIGGER'):
-        doorState = check_state()
-        if ((doorState == b'CLOSED') or (doorSTate == b'CLOSING')):
-            client.publish("homeassistant/cover/garagedoor", b'OPENING')
-        else:
-            client.publish("homeassistant/cover/garagedoor", b'CLOSING')
-        print("Turn On")
+    if (((doorState != DOORSTATE_OPENING) and (doorState != DOORSTATE_CLOSING)) and (msg.payload == MQTT_PAYLOAD_TRIGGERCMD_STOP)):
+        print("Ignoring")
+        client.publish(MQTT_CMD_STATUS, MQTT_STATUS[doorState])
+    elif ((msg.payload == MQTT_PAYLOAD_TRIGGERCMD_OPEN) or (msg.payload == MQTT_PAYLOAD_TRIGGERCMD_CLOSE) or (msg.payload == MQTT_PAYLOAD_TRIGGERCMD_STOP)):
+        doorState = check_door_state()
         RPi.GPIO.output(6,0)
         time.sleep(0.1)
         RPi.GPIO.output(6,1)
-        # client.publish("homeassistant/cover/garagedoor", b'ON')
-        # print("Back to Off")
+        if doorState == DOORSTATE_CLOSED or doorState == DOORSTATE_STOPPED_WHILE_CLOSING:
+            doorState = DOORSTATE_OPENING
+        elif doorState == DOORSTATE_OPENING:
+            doorState = DOORSTATE_STOPPED_WHILE_OPENING
+        elif doorState == DOORSTATE_OPEN or doorState == DOORSTATE_STOPPED_WHILE_OPENING:
+            doorState = DOORSTATE_CLOSING
+        elif doorState == DOORSTATE_CLOSING:
+            doorState = DOORSTATE_STOPPED_WHILE_CLOSING
+        else:
+            print("Unknown?")
 
-    elif (msg.payload == b'OFF'):
-        print("Turn Off")
-        client.publish("homeassistant/cover/garagedoor", b'OFF')
-        RPi.GPIO.output(6,1)
-    else:
-        print(msg)
+    client.publish(MQTT_CMD_STATUS, MQTT_STATUS[doorState])
+    print("UPDATED - {} {}".format(doorState,MQTT_STATUS[doorState]))
 
-def on_disconnect(client,useerdata,rc):
+
+
+def on_disconnect(client,userdata,rc):
     time.sleep(60)
     client.reconnect()
 
-client = mqtt.Client()
-client.connect("172.28.0.129",1883,60)
+#%%
 
 client.on_connect = on_connect
 client.on_message = on_message
 
-def check_state():
-        return b"CLOSED"
+#%%
+client.connect(MQTT_BROKER, MQTT_BROKERPORT, 60)
+client.loop_start()
 
 while True:
-    client.loop(1.0)
-    print("Poll here for status")
+    # client.publish(MQTT_CMD_AVAILABILITY, MQQT_PAYLOAD_AVAILABLE)
+    doorState = check_door_state(True)
+    print("{} {}".format(doorState,MQTT_STATUS[doorState]))
+    time.sleep(10)
+# %%
